4dfb6c9c1cc52c59d65ec1ec71c12df8ca061861
RingoKid <rifat.naimul@gmail.com>
2026-03-05 11:27:47 -0500

    Load Tiro Devanagari Sanskrit via Head component instead of SCSS

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -50,6 +50,9 @@ export default (() => {
             )}
           </>
         )}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Tiro+Devanagari+Sanskrit&display=swap" />
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
